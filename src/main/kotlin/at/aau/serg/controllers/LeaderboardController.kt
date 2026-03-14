fe2e925395198d8d3c2d84e86fe1a8bd4261cf29
Your Name <you@example.com>
2026-03-14 13:03:14 +0100

    Aufgabe 2.2.2 Zusätzliche Abfrage mit/ohne und ungültigen Rank

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -4,7 +4,11 @@ import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import org.springframework.http.HttpStatus
+
 
 @RestController
 @RequestMapping("/leaderboard")
@@ -13,7 +17,27 @@ class LeaderboardController(
 ) {
 
     @GetMapping
-    fun getLeaderboard(): List<GameResult> =
-        gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { it.timeInSeconds }))
+    fun getLeaderboard(@RequestParam(required = false) rank: Int?): List<GameResult>{
+        val leaderboardSorted = gameResultService.getGameResults().sortedWith(compareBy({ -it.score },
+            { it.timeInSeconds }))
+
+        //ungültiger rank, ganze Liste wird ausgegeben
+        if(rank == null){
+            return leaderboardSorted;
+        }
+
+        //ungültiger rank, Fehler HTTP 400 wird angezeigt
+        if(rank < 1 || rank > leaderboardSorted.size){
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        }
+
+        val index = rank - 1  //Ränge starten bei 1, Indexe einer Liste jedoch bei 0
+        val start = maxOf(0, index - 3) //3 Spieler vor dem Rang
+        val end = minOf(leaderboardSorted.size, index + 4) //3 Spieler über dem rang, bei +3 werden nur 2 Spieler drüber angezeigt
+
+        return leaderboardSorted.subList(start, end) //gibt die Spieler um den Rang zurück +-3
+    }
+
+
 
 }
--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -5,8 +5,10 @@ import at.aau.serg.services.GameResultService
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.springframework.web.server.ResponseStatusException
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import org.mockito.Mockito.`when` as whenever // when is a reserved keyword in Kotlin
 
 class LeaderboardControllerTests {
@@ -21,37 +23,67 @@ class LeaderboardControllerTests {
     }
 
     @Test
-    fun test_getLeaderboard_correctScoreSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
-        val second = GameResult(2, "second", 15, 10.0)
-        val third = GameResult(3, "third", 10, 15.0)
+    fun test_getLeaderboard_correctScoreSortingRankNOtThere() {
+        val a = GameResult(1, "A", 100, 20.0)
+        val b = GameResult(2, "B", 90, 15.0)
+        val c = GameResult(3, "C", 90, 14.0)
 
-        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+        whenever(mockedService.getGameResults()).thenReturn(listOf(a, b, c))
 
-        val res: List<GameResult> = controller.getLeaderboard()
+        val res = controller.getLeaderboard(null)
 
         verify(mockedService).getGameResults()
-        assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(listOf(a, c, b), res)
     }
 
     @Test
-    fun test_getLeaderboard_sameScore_CorrectIdSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
-        val second = GameResult(2, "second", 20, 10.0)
-        val third = GameResult(3, "third", 20, 15.0)
+    fun test_getLeaderboard_rankAroundPlayers() {
+        val players = listOf(
+            GameResult(1, "Anna", 150, 32.5),
+            GameResult(2, "Chris", 150, 35.0),
+            GameResult(3, "Eli", 140, 33.5),
+            GameResult(4, "Ben", 120, 30.0),
+            GameResult(5, "Dora", 100, 28.0),
+            GameResult(6, "Muje", 100, 28.0),
+        )
 
-        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+        whenever(mockedService.getGameResults()).thenReturn(players)
 
-        val res: List<GameResult> = controller.getLeaderboard()
+        val res = controller.getLeaderboard(5) // Rank = 5
 
-        verify(mockedService).getGameResults()
-        assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(listOf(players[1], players[2], players[3], players[4], players[5]), res)
+
+        @Test
+        fun test_getLeaderboard_invalidRank() {
+            whenever(mockedService.getGameResults())
+                .thenReturn(emptyList())
+
+            assertFailsWith<ResponseStatusException> {
+                controller.getLeaderboard(-1)
+            }
+        }
+
+        @Test
+        fun test_getLeaderboard_sameScore_CorrectIdSorting() {
+            val first = GameResult(1, "first", 20, 9.0)
+            val second = GameResult(2, "second", 20, 10.0)
+            val third = GameResult(3, "third", 20, 15.0)
+
+            whenever(mockedService.getGameResults()).thenReturn(listOf(first, second, third))
+
+            val res: List<GameResult> = controller.getLeaderboard(null)
+
+            verify(mockedService).getGameResults()
+            assertEquals(3, res.size)
+            assertEquals(first, res[0])
+            assertEquals(second, res[1])
+            assertEquals(third, res[2])
+        }
+        
+
+
+
+
+
     }
-
 }
